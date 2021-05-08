e04dd5ae32975626d4cba3f5c328453556e8d103
Rin Kaenbyou <ph3rin@gmail.com>
2021-05-07 22:27:20 -0700

    tests: added more tests for precedence handling

---
+++ Tests/Grammars/MinusMultDiv/ParseTest.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using Sacc;
+using static Tests.Grammars.MinusMultDiv.Symbols;
+
+namespace Tests.Grammars.MinusMultDiv
+{
+    public class ParseTest
+    {
+        private readonly ParseTable mTable =
+            new ParseTableBuilder().BuildTableForCfg(
+                new CfgBuilder()
+                    .AddAllProductionsInClass<A>()
+                    .AddAllProductionsInClass<Expr>()
+                    .AddAllProductionsInClass<Minus>()
+                    .AddAllProductionsInClass<Mult>()
+                    .AddAllProductionsInClass<Div>()
+                    .DeclarePrecedence(Symbol.Of<Minus>())
+                    .DeclarePrecedence(Symbol.Of<Mult>(), Symbol.Of<Div>())
+                    .Build());
+
+        [Test]
+        public void ThreeNumbers()
+        {
+            var minus = new Minus();
+            var node = mTable.Parse(new[]
+            {
+                Node.Make(new A(1)),
+                Node.Make(minus),
+                Node.Make(new A(2)),
+                Node.Make(minus),
+                Node.Make(new A(3))
+            });
+
+            Assert.AreEqual(-4, (node.Payload as Expr)?.Eval());
+        }
+
+        [Test]
+        public void MinusMultDiv()
+        {
+            var minus = Node.Make(new Minus());
+            var mul = Node.Make(new Mult());
+            var div = Node.Make(new Div());
+            var node = mTable.Parse(new[]
+            {
+                Node.Make(new A(5)),
+                minus,
+                Node.Make(new A(3)),
+                div,
+                Node.Make(new A(2)),
+                mul,
+                Node.Make(new A(5))
+            });
+            
+            Assert.AreEqual(0, (node.Payload as Expr)?.Eval());
+        }
+        
+        [Test]
+        public void MinusMultMinus()
+        {
+            var minus = Node.Make(new Minus());
+            var mul = Node.Make(new Mult());
+            var node = mTable.Parse(new[]
+            {
+                Node.Make(new A(5)),
+                minus,
+                Node.Make(new A(3)),
+                mul,
+                Node.Make(new A(2)),
+                mul,
+                Node.Make(new A(1)),
+                minus,
+                Node.Make(new A(-1))
+            });
+            
+            Assert.AreEqual(0, (node.Payload as Expr)?.Eval());
+        }
+    }
+}
---
+++ Tests/Grammars/MinusMultDiv/Symbols.cs
@@ -0,0 +1,119 @@
+﻿using Sacc;
+
+namespace Tests.Grammars.MinusMultDiv
+{
+    public static class Symbols
+    {
+        [StartSymbol]
+        public abstract class Expr
+        {
+            public abstract int Eval();
+
+            private class SubExpr : Expr
+            {
+                public Expr Lhs { get; }
+                public Expr Rhs { get; }
+
+                public SubExpr(Expr lhs, Expr rhs)
+                {
+                    Lhs = lhs;
+                    Rhs = rhs;
+                }
+
+                public override int Eval()
+                {
+                    return Lhs.Eval() - Rhs.Eval();
+                }
+            }
+
+            private class MultExpr : Expr
+            {
+                public Expr Lhs { get; }
+                public Expr Rhs { get; }
+
+                public MultExpr(Expr lhs, Expr rhs)
+                {
+                    Lhs = lhs;
+                    Rhs = rhs;
+                }
+
+                public override int Eval()
+                {
+                    return Lhs.Eval() * Rhs.Eval();
+                }
+            }
+            
+            private class DivExpr : Expr
+            {
+                public Expr Lhs { get; }
+                public Expr Rhs { get; }
+
+                public DivExpr(Expr lhs, Expr rhs)
+                {
+                    Lhs = lhs;
+                    Rhs = rhs;
+                }
+
+                public override int Eval()
+                {
+                    return Lhs.Eval() / Rhs.Eval();
+                }
+            }
+            
+            [Production]
+            public static Expr SingleA(A a)
+            {
+                return a;
+            }
+
+            [Production]
+            public static Expr A_Sub_A(Expr lhs, Minus m, Expr rhs)
+            {
+                return new SubExpr(lhs, rhs);
+            }
+
+            [Production]
+            public static Expr A_Mult_A(Expr lhs, Mult m, Expr rhs)
+            {
+                return new MultExpr(lhs, rhs);
+            }
+
+            [Production]
+            public static Expr A_Div_A(Expr lhs, Div d, Expr rhs)
+            {
+                return new DivExpr(lhs, rhs);
+            }
+        }
+        
+        public class A : Expr
+        {
+            public int Val { get; }
+
+            public A(int val)
+            {
+                Val = val;
+            }
+
+            public override int Eval()
+            {
+                return Val;
+            }
+        }
+        
+        [Associativity(Associativity.Left)]
+        public class Minus
+        {
+        }
+
+        [Associativity(Associativity.Left)]
+        public class Mult
+        {
+        }
+
+        [Associativity(Associativity.Left)]
+        public class Div
+        {
+        }
+        
+    }
+}
